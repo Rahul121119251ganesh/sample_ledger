b02a1386190ee399823f098ea5331e940444565e
Rahul ganesh <86249548+Rahul121119251ganesh@users.noreply.github.com>
2026-05-21 17:24:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const SUPABASE_URL = 'https://iojcfzxwafdbnhxfgffe.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvamNmenh3YWZkYm5oeGZnZmZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyNzYyOTcsImV4cCI6MjA5NDg1MjI5N30.2Ao6_cxFev400bf8MB8831zUdcihsKIwdhw_ezFPFlE';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = window.supabaseClient || 
+    window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const supabase = window.supabaseClient;
 let currentUser = null; // Stores the logged-in username string
 
 // --- State Management ---
